80644d91cd78cf4531268a0adab79c00b2894b7b
Oteri Eyenike <teyenike1@gmail.com>
2024-04-03 13:36:51 +0100

    Feat: get info about the weather and design of the website

---
+++ app.py
@@ -0,0 +1,37 @@
+from flask import Flask, render_template, request
+from weather import main as get_weather
+from infobip_channels.sms.channel import SMSChannel
+
+import os
+
+app = Flask(__name__)
+
+@app.route("/", methods=["GET", "POST"])
+def index():
+  data = None
+  degree_sign = u"\N{DEGREE SIGN}"
+  if request.method == "POST":
+    city_name = request.form.get("cityName")
+    state_code = request.form.get("stateName")
+    country_code = request.form.get("countryName")
+    data = get_weather(city_name, state_code, country_code)
+    if city_name and state_code and country_code:
+      data = get_weather(city_name, state_code, country_code)
+      message = f"Weather for {city_name}: temperature of {data.temperature}{degree_sign}C and {data.description}"
+      # send_sms_from_app(message)
+      print(message)
+  return render_template("index.html", data=data)
+
+
+def send_sms_from_app(text):
+    channel = SMSChannel.from_env()
+    sms_response = channel.send_sms_message({
+        'messages': [{
+            'from': 'Real-time weather info',
+            'text': text,
+            'destinations': [{
+                'to': os.environ['DESTINATION_NUMBER']
+            }],
+        }]
+    })
+    print(sms_response)
---
+++ weather.py
@@ -0,0 +1,59 @@
+import requests
+import os
+from dotenv import load_dotenv
+from dataclasses import dataclass
+
+
+load_dotenv()
+API_KEY = os.environ["OWM_API_KEY"]
+OWM_ENDPOINT = "https://api.openweathermap.org/data/2.5/weather"
+
+@dataclass
+class WeatherData:
+    main: str
+    description: str
+    icon: str
+    temperature: int
+
+def get_lat_lon(city_name, state_code, country_code, API_key):
+    resp = requests.get(f'http://api.openweathermap.org/geo/1.0/direct?q={city_name},{state_code},{country_code}&appid={API_key}')
+    data = resp.json()
+    if data:
+        location = data[0]
+        return location.get('lat'), location.get('lon')
+    else:
+        return "Invalid lat and lon response"
+
+
+def get_current_weather(lat, lon, API_key):
+    params = {
+        "lat": lat,
+        "lon": lon,
+        "appid": API_key,
+        "units": "metric"
+    }
+    resp = requests.get(OWM_ENDPOINT, params=params)
+    data = resp.json()
+    weather = data["weather"][0]
+    main = weather["main"]
+    description = weather["description"]
+    icon = weather["icon"]
+    temperature = int(data["main"]["temp"])
+    return WeatherData(main, description, icon, temperature)
+
+
+def main(city_name, state_name, country_name):
+    lat, lon = get_lat_lon(city_name, state_name, country_name, API_KEY)
+    weather_data = get_current_weather(lat, lon, API_KEY)
+    return weather_data
+
+if __name__ == "__main__":
+    lat, lon = get_lat_lon("Miami", "FL", "US", API_KEY)
+    if lat is not None and lon is not None:
+        weather_data = get_current_weather(lat, lon, API_KEY)
+        if weather_data is not None:
+            print(weather_data)
+        else:
+            print("Failed to retrieve weather data.")
+    else:
+        print("Failed to retrieve latitude and longitude.")
